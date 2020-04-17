1710c625f5eded5934b680eaa437a032065073f1
shinixsensei <crezoxplays@gmail.com>
2020-04-17 17:44:44 +0200

    level add

--- src/main/java/Commands/rRank.java
+++ src/main/java/Commands/rRank.java
@@ -16,33 +16,39 @@ public class rRank implements Command {
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
 
-        String balance;
+        int balance, level;
         if(event.getMessage().getMentionedUsers().size() >= 1) {
             if(CounterEnv.propExist("xp" + event.getMessage().getMentionedUsers().get(0).getId())) {
-                balance = CounterEnv.getValue("xp" + event.getMessage().getMentionedUsers().get(0).getId());
+                balance = Integer.parseInt(CounterEnv.getValue("xp" + event.getMessage().getMentionedUsers().get(0).getId()));
             }else {
-                balance = "0";
+                balance = 0;
             }
+
+            level = balance / 1000;
 
             event.getTextChannel().sendMessage(
                     new EmbedBuilder()
                             .setTitle("XP for " + event.getMessage().getMentionedUsers().get(0).getAsTag() + "!")
                             .setColor(Color.PINK)
                             .addField("Raw XP", "**" + balance + "**", true)
+                            .addField("Level", "**" + level + "**", true)
                             .build()
             ).queue();
         }else {
             if(CounterEnv.propExist("xp" + event.getAuthor().getId())) {
-                balance = CounterEnv.getValue("xp" + event.getAuthor().getId());
+                balance = Integer.parseInt(CounterEnv.getValue("xp" + event.getAuthor().getId()));
             }else {
-                balance = "0";
+                balance = 0;
             }
+
+            level = balance / 1000;
 
             event.getTextChannel().sendMessage(
                     new EmbedBuilder()
                             .setTitle("Your current XP, " + event.getAuthor().getAsTag() + "!")
                             .setColor(Color.PINK)
                             .addField("Raw XP", "**" + balance + "**", true)
+                            .addField("Level", "**" + level + "**", true)
                             .build()
             ).queue();
         }
